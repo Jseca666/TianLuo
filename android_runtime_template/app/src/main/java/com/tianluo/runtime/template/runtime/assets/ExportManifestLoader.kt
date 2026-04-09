a40d44ce8e94cb31084d0dfc8e0d4fd7fc0327f0
dululu <75114030+Jseca666@users.noreply.github.com>
2026-04-09 11:38:49 +0800

    feat(v2): add export manifest loader placeholder

---
+++ android_runtime_template/app/src/main/java/com/tianluo/runtime/template/runtime/assets/ExportManifestLoader.kt
@@ -0,0 +1,7 @@
+package com.tianluo.runtime.template.runtime.assets
+
+class ExportManifestLoader {
+    fun load(): ExportManifestAsset {
+        throw UnsupportedOperationException("Export manifest loading is not implemented yet")
+    }
+}
