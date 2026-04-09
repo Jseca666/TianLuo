c46e9d0b2e802733fe20a1ea07f6f87fc67d9ed2
dululu <75114030+Jseca666@users.noreply.github.com>
2026-04-09 11:25:49 +0800

    feat(v3): add asset layout constants

---
+++ apk_exporter/asset_layout.py
@@ -0,0 +1,8 @@
+class AssetLayout:
+    """Android assets 输出目录约定。"""
+
+    ROOT = "automation"
+    LOCATORS_DIR = "automation/locators"
+    TEMPLATES_DIR = "automation/templates"
+    MASKS_FILE = "automation/masks.json"
+    EXPORT_MANIFEST = "automation/export_manifest.json"
